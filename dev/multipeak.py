347a9eff710242a273d5b13231edf653a7d9739c
Yuan Wei <yuan.wei@psi.ch>
2022-04-20 11:52:28 +0200

    04/20/2022

--- dev/multipeak.py
+++ dev/multipeak.py
@@ -1,33 +1,39 @@
 import numpy as np
+from scipy import optimize
+# import matplotlib as plt
+
+__all__ = [
+        "multipeakes"
+        ]
 
 
-def gauss_norm(x, xc, sigma, area):
+def Gauss_norm(x, xc, sigma, area):
     norm = 1.0 / sigma / 2 / np.sqrt(np.pi / 2)
     return area * norm * np.exp(-2 * (x - xc) ** 2 / (sigma * 2) ** 2)
 
 
-def gaussian_norm(x, xc, fwhm, area):
+def Gaussian_norm(x, xc, fwhm, area):
     norm = 1.0 / fwhm / np.sqrt(np.pi / 4 / np.log(2))
     return area * norm * np.exp(-4 * np.log(2) * (x - xc) ** 2 / fwhm ** 2)
 
 
-def gauss_amp(x, xc, sigma, amp):
+def Gauss_amp(x, xc, sigma, amp):
     return amp * np.exp(-2 * (x - xc) ** 2 / (sigma * 2) ** 2)
 
 
-def gaussian_amp(x, xc, fwhm, amp):
+def Gaussian_amp(x, xc, fwhm, amp):
     return amp * np.exp(-4 * np.log(2) * (x - xc) ** 2 / fwhm ** 2)
 
 
-def lorentz_norm(x, xc, fwhm, area):
+def Lorentz_norm(x, xc, fwhm, area):
     return 2 * area / np.pi * fwhm / (4 * (x - xc) ** 2 + fwhm ** 2)
 
 
-def lorentz_amp(x, xc, fwhm, amp):
+def Lorentz_amp(x, xc, fwhm, amp):
     return amp * fwhm ** 2 / (4 * (x - xc) ** 2 + fwhm ** 2)
 
 
-def Psd_Voigt(x, xc, w, mu, area):
+def PsdVoigt(x, xc, w, mu, area):
     return area * (
         mu * 2 / np.pi * w / (4 * (x - xc) ** 2 + w ** 2)
         + (1 - mu)
@@ -38,7 +44,7 @@ def Psd_Voigt(x, xc, w, mu, area):
     )
 
 
-def Psd_Voigt2(x, xc, wl, wg, mu, area):
+def PsdVoigt2(x, xc, wl, wg, mu, area):
     return area * (
         mu * 2 / np.pi * wl / (4 * (x - xc) ** 2 + wl ** 2)
         + (1 - mu)
@@ -47,3 +53,8 @@ def Psd_Voigt2(x, xc, wl, wg, mu, area):
         / wg
         * np.exp(-4 * np.log(2.0) * (x - xc) ** 2 / wg ** 2)
     )
+
+def multipeaks(peakType,peakNumber):
+    for x in range(peakNumber):
+        peaks = peaks + peakType()
+    def callback_function():
