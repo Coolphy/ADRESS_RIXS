25c8a5ec43b45cd2b7b93eb127202c00fd54e04d
Yuan Wei <yuan.wei@psi.ch>
2022-12-31 20:38:06 +0100

    12/31/2022

--- dev/rixsplot/rixsplot.py
+++ dev/rixsplot/rixsplot.py
@@ -1,58 +1,36 @@
-import sys
+# -*- coding: utf-8 -*-
 
-from PySide6.QtCore import (
-    QCoreApplication,
-    QDate,
-    QDateTime,
-    QLocale,
-    QMetaObject,
-    QObject,
-    QPoint,
-    QRect,
-    QSize,
-    QTime,
-    QUrl,
-    Qt,
-)
-from PySide6.QtGui import (
-    QBrush,
-    QColor,
-    QConicalGradient,
-    QCursor,
-    QFont,
-    QFontDatabase,
-    QGradient,
-    QIcon,
-    QImage,
-    QKeySequence,
-    QLinearGradient,
-    QPainter,
-    QPalette,
-    QPixmap,
-    QRadialGradient,
-    QTransform,
-)
-from PySide6.QtWidgets import (
-    QApplication,
-    QGridLayout,
-    QHBoxLayout,
-    QHeaderView,
-    QLabel,
-    QLineEdit,
-    QListWidget,
-    QListWidgetItem,
-    QMainWindow,
-    QPushButton,
-    QSizePolicy,
-    QSpacerItem,
-    QTabWidget,
-    QTableWidget,
-    QTableWidgetItem,
-    QTextBrowser,
-    QVBoxLayout,
-    QWidget,
-)
-import pyqtgraph as pg
+################################################################################
+## Form generated from reading UI file 'uieVoYJS.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+import sys
+import os
+
+import numpy as np
+import matplotlib
+
+matplotlib.use("Qt5Agg")
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
+from rixs_tools import *
+
+
+class MplCanvas(FigureCanvas):
+    def __init__(self, parent=None):
+        fig = Figure()
+        self.axes = fig.add_subplot(111)
+        super(MplCanvas, self).__init__(fig)
 
 
 class Ui_MainWindow(object):
@@ -90,6 +68,9 @@ class Ui_MainWindow(object):
         self.verticalLayout.setObjectName("verticalLayout")
         self.listWidget = QListWidget(self.tab_1)
         self.listWidget.setObjectName("listWidget")
+        self.listWidget.setSelectionMode(
+            QAbstractItemView.SelectionMode.ExtendedSelection
+        )
 
         self.verticalLayout.addWidget(self.listWidget)
 
@@ -130,6 +111,7 @@ class Ui_MainWindow(object):
 
         self.lineEdit_2 = QLineEdit(self.tab_2)
         self.lineEdit_2.setObjectName("lineEdit_2")
+        self.lineEdit_2.setText("0")
 
         self.verticalLayout_2.addWidget(self.lineEdit_2)
 
@@ -145,6 +127,7 @@ class Ui_MainWindow(object):
 
         self.lineEdit_3 = QLineEdit(self.tab_2)
         self.lineEdit_3.setObjectName("lineEdit_3")
+        self.lineEdit_3.setText("1")
 
         self.verticalLayout_2.addWidget(self.lineEdit_3)
 
@@ -179,12 +162,19 @@ class Ui_MainWindow(object):
         self.tabWidget_2.setObjectName("tabWidget_2")
         self.tab_3 = QWidget()
         self.tab_3.setObjectName("tab_3")
-        self.gridLayout_6 = QGridLayout(self.tab_3)
-        self.gridLayout_6.setObjectName("gridLayout_6")
-        self.graphWidget = pg.PlotWidget(self.tab_3)
-        self.graphWidget.setObjectName("graphWidget")
+        self.gridLayout_3 = QGridLayout(self.tab_3)
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.verticalLayout_3 = QVBoxLayout()
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
 
-        self.gridLayout_6.addWidget(self.graphWidget, 0, 0, 1, 1)
+        self.mplCanvas = MplCanvas(self)
+        self.mplCanvas.setObjectName("mplCanvas")
+        self.mplToolbar = NavigationToolbar(self.mplCanvas, MainWindow)
+        self.mplToolbar.setObjectName("mplToolbar")
+
+        self.verticalLayout_3.addWidget(self.mplToolbar)
+        self.verticalLayout_3.addWidget(self.mplCanvas)
+        self.gridLayout_3.addLayout(self.verticalLayout_3, 0, 0, 1, 1)
 
         self.tabWidget_2.addTab(self.tab_3, "")
         self.tab_4 = QWidget()
@@ -208,8 +198,9 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
+        self.connectUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
         self.tabWidget_2.setCurrentIndex(0)
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -250,18 +241,130 @@ class Ui_MainWindow(object):
         )
         self.tabWidget.setTabText(
             self.tabWidget.indexOf(self.tab_2),
-            QCoreApplication.translate("MainWindow", "More", None),
+            QCoreApplication.translate("MainWindow", "Process", None),
         )
         self.tabWidget_2.setTabText(
             self.tabWidget_2.indexOf(self.tab_3),
-            QCoreApplication.translate("MainWindow", "Plot", None),
+            QCoreApplication.translate("MainWindow", "Spectrum", None),
         )
         self.tabWidget_2.setTabText(
             self.tabWidget_2.indexOf(self.tab_4),
             QCoreApplication.translate("MainWindow", "Info", None),
         )
 
-    # retranslateUi
+    def connectUi(self, MainWindow):
+        self.pushButton.clicked.connect(self.do_open)
+        self.lineEdit.editingFinished.connect(self.lineEdit_finish)
+        self.pushButton_2.clicked.connect(self.do_plot)
+        self.pushButton_3.clicked.connect(self.do_correlate)
+        self.pushButton_4.clicked.connect(self.do_sum)
+        self.pushButton_5.clicked.connect(self.do_shift)
+        self.pushButton_6.clicked.connect(self.do_etrans)
+
+        self.pushButton_8.clicked.connect(self.do_save)
+
+        self.mplCanvas.mpl_connect("button_press_event", self.mpl_on_click)
+
+    def do_open(self):
+        self.path = QFileDialog.getExistingDirectory()
+        self.lineEdit.setText(self.path)
+        self.listWidget.clear()
+        list = sorted(os.listdir(self.path))
+        self.listWidget.addItems(list)
+
+    def lineEdit_finish(self):
+        self.path = self.lineEdit.text()
+        self.listWidget.clear()
+        list = sorted(os.listdir(self.path))
+        self.listWidget.addItems(list)
+
+    def do_plot(self):
+        self.items = self.listWidget.selectedItems()
+        self.data = {}
+        self.rt = rixs_tools()
+        for item in self.items:
+            self.data[item.text()] = self.rt.load_ccd(f"{self.path}/{item.text()}")
+        self.plot_multi_lines(self.data)
+        self.mplCanvas.axes.set_xlabel("Pixels")
+        self.mplCanvas.axes.set_ylabel("Counts")
+        self.mplCanvas.draw()
+
+    def do_correlate(self):
+        xrange = self.mplCanvas.axes.get_xlim()
+        yrange = self.mplCanvas.axes.get_ylim()
+        print(xrange)
+        for i, item in enumerate(self.items):
+            if i == 0:
+                refdata = self.data[item.text()]
+            else:
+                tempdata = self.data[item.text()]
+                self.data[item.text()] = self.rt.x_corr(
+                    refdata, tempdata, xrange=xrange
+                )
+        self.plot_multi_lines(self.data)
+        self.mplCanvas.axes.set_xlim(xrange)
+        self.mplCanvas.axes.set_ylim(yrange)
+        self.mplCanvas.axes.set_xlabel("Pixels")
+        self.mplCanvas.axes.set_ylabel("Counts")
+        self.mplCanvas.draw()
+
+    def do_sum(self):
+        for i, key in enumerate(self.data):
+            if i == 0:
+                self.ydata = self.data[key]
+            else:
+                self.ydata = self.ydata + self.data[key]
+        self.xdata = np.arange(len(self.ydata))
+        self.plot_one_line(self.xdata, self.ydata)
+        self.mplCanvas.axes.set_xlabel("Pixels")
+        self.mplCanvas.axes.set_ylabel("Counts")
+        self.mplCanvas.draw()
+
+    def do_shift(self):
+        shifts = float(self.lineEdit_2.text())
+        self.xdata = self.xdata - shifts
+        self.plot_one_line(self.xdata, self.ydata)
+        self.mplCanvas.axes.set_xlabel("Pixel Transfer")
+        self.mplCanvas.axes.set_ylabel("Counts")
+        self.mplCanvas.draw()
+
+    def do_etrans(self):
+        dispersion = float(self.lineEdit_3.text())
+        self.xdata = self.xdata * dispersion
+        self.plot_one_line(self.xdata, self.ydata)
+        self.mplCanvas.axes.set_xlabel("Energy Transfer")
+        self.mplCanvas.axes.set_ylabel("Counts")
+        self.mplCanvas.draw()
+
+    def do_save(self):
+        if self.ydata is not None:
+            savefile, _ = QFileDialog.getSaveFileName(dir=f"", filter="Text (*.txt)")
+            if savefile == "":
+                pass
+            else:
+                data = np.array([self.xdata, self.ydata])
+                np.savetxt(
+                    savefile,
+                    np.transpose(data),
+                    delimiter="\t",
+                    header="Energy\tCounts",
+                    comments="#",
+                )
+
+    def plot_one_line(self, xdata, ydata):
+        self.mplCanvas.axes.cla()
+        self.mplCanvas.axes.plot(xdata, ydata, lw=0.5)
+        self.mplCanvas.draw()
+
+    def plot_multi_lines(self, data):
+        self.mplCanvas.axes.cla()
+        for key in data:
+            self.mplCanvas.axes.plot(data[key], lw=0.5)
+        self.mplCanvas.draw()
+
+    def mpl_on_click(self, event):
+        ix, iy = event.xdata, event.ydata
+        self.lineEdit_2.setText(f"{ix:.4f}")
 
 
 if __name__ == "__main__":
