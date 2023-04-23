d9f58ddde47e7e76fdea903d495a65a1a84d56f4
Yuan Wei <yuan.wei@psi.ch>
2023-04-23 12:39:24 +0200

    04/23/2023

--- calq/cal_q_math.py
+++ calq/cal_q_math.py
@@ -19,38 +19,45 @@ ar = 2 * pi / (a * sin(pi - gamma))
 # (1,1)
 aar = sqrt(ar**2 * (2 - 2 * cos(gamma)))
 
+# (0,1)
+cr = 2 * pi / (a * sin(pi - gamma))
+
+# %%
+# q out
+qout = k_out_plane / ar
+simplify(qout)
+
+qoutvalue = qout.subs(
+    [
+        (twotheta, 130 / 180 * pi),
+        (a, 7.9),
+        (gamma, 90 / 180 * pi),
+        (e, 576),
+        # (specular, 65 / 180 * pi),
+        # (theta, 40),
+    ]
+)
+
+print(simplify(qoutvalue.evalf(4)))
+
 # (1,0)
 
 # %%
 q10 = k_in_plane / ar
 simplify(q10)
 
-# %%
 q10value = q10.subs(
     [
-        (twotheta, 150 / 180 * pi),
-        (a, 3.9536),
-        (gamma, 120 / 180 * pi),
-        (e, 708),
+        (twotheta, 130 / 180 * pi),
+        (a, 3.5),
+        (gamma, 90 / 180 * pi),
+        (e, 576),
         # (specular, 65 / 180 * pi),
         # (theta, 40),
     ]
 )
 
 print(simplify(q10value.evalf(4)))
-
-q01value = q10.subs(
-    [
-        (twotheta, 150 / 180 * pi),
-        (a, 16.396),
-        (gamma, 90 / 180 * pi),
-        (e, 708),
-        (specular, 65 / 180 * pi),
-        # (theta, 40),
-    ]
-)
-
-print(simplify(q01value.evalf(4)))
 
 # simplify(q01value)
 # %%
@@ -59,7 +66,6 @@ print(simplify(q01value.evalf(4)))
 q11 = k_in_plane / aar
 simplify(q11)
 
-# %%
 q11value = q11.subs(
     [
         (twotheta, 130 / 180 * pi),
@@ -83,8 +89,8 @@ import math
 
 thetaarray = np.arange(0, 130, 1) * pi / 180
 
-x = 0.3545 * np.sin(65.0 * math.pi / 180.0 - thetaarray.astype(float))
-y = 1.697 * np.cos(65.0 * math.pi / 180.0 - thetaarray.astype(float))
+x = 0.2948 * np.sin(65.0 * math.pi / 180.0 - thetaarray.astype(float))
+y = 0.6654 * np.cos(65.0 * math.pi / 180.0 - thetaarray.astype(float))
 
 rect = [0.1, 0.1, 0.8, 0.8]
 
@@ -92,12 +98,14 @@ fig = plt.figure()
 
 axs = fig.add_axes(rect)
 # axp = fig.add_axes(axs.get_position().bounds, polar=True, frameon=False)
-fig.gca().set_aspect(0.3545/1.697)
+fig.gca().set_aspect(0.2948/0.6654)
 axs.plot(x, y)
 axs.set_xlim([-0.5, 0.5])
-axs.set_ylim([0, 2])
+axs.set_ylim([0, 1])
 axs.grid()
 axs.set_xlabel("[H, 0 ,0 ]")
 axs.set_ylabel("[0, 0 ,L ]")
+
+axs.scatter([0],[0.5])
 
 # %%
